cec8f0e7593f343010a61ac2c8c06d9a241f40cc
dukepat <dukepato2010@gmail.com>
2022-04-23 23:41:21 +0100

    deploy Script

--- hardhat_exercise/contracts/Whitelist.sol
+++ hardhat_exercise/contracts/Whitelist.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.0;
 contract Whitelist {
 
     unit8 public maxWhitelistedAddresses;
+
+    uint8 public numAddressesWhitelisted;
+
+    mapping(address => bool) public whitelistedAddresses;
+    
     constructor(uint8 _maxWhitelistedAddresses) {
         maxWhitelistedAddresses = _maxwhitelistedAddresses;
         
